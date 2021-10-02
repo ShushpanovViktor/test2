fd5d138b2baf14f92109cf291eef0444e72a892b
Viktor Shushpanov <bicompton21@yandex.ru>
2021-10-02 20:50:18 +0300

    test2

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        Integer a = 1;
+        Integer b = 2;
+
+        linkedList.add(a);
+        linkedList.add(b);
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(a);
+        arrayList.add(b);
+
+        for (int i = 0; i < linkedList.size(); i++) {
+            System.out.println(linkedList.get(i));
+        }
+
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.println(arrayList.get(i));
+        }
+    }
+
+}
